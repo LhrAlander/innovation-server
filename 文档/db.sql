8aad96e55aed0f23bacb08f2c6a07ec7d098cf05
wanghan <wanghan@SMacBookAir.local>
2018-01-12 17:07:06 +0800

    更改sql

--- 文档/db.sql
+++ 文档/db.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `academy`
 --
-use `innovation_db`
+
 DROP TABLE IF EXISTS `academy`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
