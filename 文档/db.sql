d0eb94b09f800ba3024acdd9845baa77bd3d7d82
LhrAlander <1147816814@qq.com>
2018-01-12 17:02:40 +0800

    更改sql

--- 文档/db.sql
+++ 文档/db.sql
@@ -18,7 +18,6 @@
 --
 -- Table structure for table `academy`
 --
-use `innovation_db`
 DROP TABLE IF EXISTS `academy`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
